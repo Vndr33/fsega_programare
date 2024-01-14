67ce4cc4bc955526d590079653c6f5815c780f25
Iulia Pop <andreeapopmonica@gmail.com>
2024-01-15 00:40:15 +0200

    first commit

---
+++ Laboratoare/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Laboratoare
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Laboratoare/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Laboratoare
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
