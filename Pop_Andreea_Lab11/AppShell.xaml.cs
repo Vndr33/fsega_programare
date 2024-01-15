7182530056005f20ae0614edf7e3a45b3f59bba7
Iulia Pop <andreeapopmonica@gmail.com>
2024-01-15 21:05:57 +0200

    second try

---
+++ Pop_Andreea_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pop_Andreea_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pop_Andreea_Lab11/Data/IRestService.cs
@@ -0,0 +1,12 @@
+﻿using Pop_Andreea_Lab11.Models;
+
+namespace Pop_Andreea_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+
+    }
+}
